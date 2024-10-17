559f23c94e841ca31a8455d11c536fa29e77cb93
tom2almighty <bboyc80@gmail.com>
2024-10-17 10:12:00 +0800

    Update moze.js

--- Script/moze.js
+++ Script/moze.js
@@ -9,31 +9,36 @@
 *************************************/
 
 
-const chxm1023 = {};
-const chxm1024 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+const responseData = {};
+const headers = $request.headers;
+const parsedResponse = JSON.parse(typeof $response != "undefined" && $response.body || null);
+const userAgent = headers['User-Agent'] || headers['user-agent'];
+
 if (typeof $response == "undefined") {
-  delete $request.headers["x-revenuecat-etag"];
-  delete $request.headers["X-RevenueCat-ETag"];
-  chxm1023.headers = $request.headers;
-} else if (chxm1024 && chxm1024.subscriber) {
-  chxm1024.subscriber.subscriptions = chxm1024.subscriber.subscriptions || {};
-  chxm1024.subscriber.entitlement = chxm1024.subscriber.entitlement || {};
-  const app = 'cm';const list = {'cm':{name: 'MOZE_PREMIUM_SUBSCRIPTION', id: 'MOZE_PRO_SUBSCRIPTION_YEARLY_BASIC'}};
-  const data = {
-   "Author": "chxm1023",
-   "Telegram" : "https://t.me/chxm1023",
-   "warning": "仅供学习，禁止转载或售卖",
-   "original_purchase_date": "2022-09-09T09:09:09Z",
-   "purchase_date": "2022-09-09T09:09:09Z",
-   "ownership_type": "PURCHASED"};
-for (const i in list) { if (new RegExp(`^${i}`, `i`).test(app)) {
-  chxm1024.subscriber.subscriptions[list[i].id] = data;
-  chxm1024.subscriber.entitlements[list[i].name] = JSON.parse(JSON.stringify(data));
-  chxm1024.subscriber.entitlements[list[i].name].product_identifier = list[i].id;
-                break;
-          }
-    }
-    chxm1023.body = JSON.stringify(chxm1024);
+  delete headers["x-revenuecat-etag"];
+  delete headers["X-RevenueCat-ETag"];
+  responseData.headers = headers;
+} else if (parsedResponse && parsedResponse.subscriber) {
+  parsedResponse.subscriber.subscriptions = parsedResponse.subscriber.subscriptions || {};
+  parsedResponse.subscriber.entitlements = parsedResponse.subscriber.entitlements || {};
+  
+  if (new RegExp(`^MOZE`, `i`).test(userAgent)) {
+    const data = {  "purchase_date" : "2023-09-09T09:09:09Z",  "expires_date" : "2099-09-09T09:09:09Z" };
+    const name = 'MOZE_PREMIUM_SUBSCRIPTION';
+    const ids = 'MOZE_PRO_SUBSCRIPTION_YEARLY_BASIC';
+    
+    parsedResponse.subscriber.entitlements[name] = Object.assign({}, data, { product_identifier: ids });
+    
+    const subData = Object.assign({}, data, {
+      "original_purchase_date": "2023-09-09T09:09:09Z",
+      "store": "app_store",
+      "ownership_type": "PURCHASED"
+    });
+    parsedResponse.subscriber.subscriptions[ids] = subData;
+    
+    responseData.body = JSON.stringify(parsedResponse);
+    console.log('MOZE解锁成功');
+  }
 }
 
-$done(chxm1023);
+$done(responseData);
