3792d64670b079e160b6e85715e219704e9fabaf
tom2almighty <30921462+tom2almighty@users.noreply.github.com>
2025-05-15 10:38:39 +0800

    Update vvebo.js

--- Script/vvebo.js
+++ Script/vvebo.js
@@ -9,7 +9,7 @@ if (url.includes("remind/unread_count")) {
 } else if (url.includes("statuses/user_timeline")) {
   let uid = getUid(url) || $persistentStore.read("uid");
   url = url.replace("statuses/user_timeline", "profile/statuses/tab").replace("max_id", "since_id");
-  url = url + `&containerid=230413${uid}_-_WEIBO_SECOND_PROFILE_WEIBO`;
+  url = url + &containerid=230413${uid}_-_WEIBO_SECOND_PROFILE_WEIBO;
   $done({ url });
 } else if (url.includes("profile/statuses/tab")) {
   let data = JSON.parse($response.body);
@@ -21,6 +21,10 @@ if (url.includes("remind/unread_count")) {
     .map((status) => (status.isTop ? { ...status, label: "置顶" } : status));
   let sinceId = data.cardlistInfo.since_id;
   $done({ body: JSON.stringify({ statuses, since_id: sinceId, total_number: 100 }) });
+} if (url.includes("selffans")) {
+  let data = JSON.parse($response.body);
+  let cards = data.cards.filter((card) => card.itemid !== "INTEREST_PEOPLE2");
+  $done({ body: JSON.stringify({ ...data, cards }) });
 } else {
   $done({});
 }
